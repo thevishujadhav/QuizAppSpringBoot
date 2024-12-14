da52252d45de313ee4f3f24d8501ac69f12d8052
vishu <vishuj802@gmail.com>
2024-12-15 00:42:30 +0530

    first commit

---
+++ src/main/java/com/quiz/quizapp/model/Quiz.java
@@ -0,0 +1,43 @@
+package com.quiz.quizapp.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+
+    @ManyToMany
+    private List<Question> questions;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+}
---
+++ src/main/java/com/quiz/quizapp/service/QuestionService.java
@@ -0,0 +1,58 @@
+package com.quiz.quizapp.service;
+
+import com.quiz.quizapp.dao.QuestionDao;
+import com.quiz.quizapp.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+
+public class QuestionService {
+
+
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findBycategory(category),HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        questionDao.save(question);
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<String> deleteQuestion(Integer id) {
+        questionDao.deleteById(id);
+        return new ResponseEntity<>("success",HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> updateQuestion(Question question) {
+
+        questionDao.save(question);
+        return new ResponseEntity<>("success",HttpStatus.OK);
+
+
+    }
+}
